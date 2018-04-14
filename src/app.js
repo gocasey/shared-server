d916bd14727edf8b03effd5217d9c7703303d234
gocasey <gonzak6@gmail.com>
2018-04-14 00:25:45 -0300

    Removing morgan as logger

--- src/app.js
+++ src/app.js
@@ -1,36 +1,21 @@
 const config = require('config');
 const cors = require('cors');
 const express = require('express');
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const logger = require('./utils/logger.js');
+const Logger = require('./utils/logger.js');
 const filesRouter = require('./middlewares/routers/files_router.js');
 const serversRouter = require('./middlewares/routers/servers_router.js');
 const usersRouter = require('./middlewares/routers/users_router.js');
 const ConnectionPoolFactory = require('./lib/factories/connection_pool_factory.js');
-const postgrePool = new ConnectionPoolFactory().createPool();
 const errorMiddleware = require('./middlewares/error-middleware');
+
+const logger = new Logger();
+const postgrePool = new ConnectionPoolFactory(logger).createPool();
+
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(morgan('dev', {
-  skip: function(req, res) {
-    return res.statusCode < 400;
-  },
-  stream: function(msg) {
-    logger.error(msg);
-  },
-}));
-
-app.use(morgan('dev', {
-  skip: function(req, res) {
-    return res.statusCode >= 400;
-  },
-  stream: function(msg) {
-    logger.info(msg);
-  },
-}));
 
 // Enable CORS for all routes
 app.use(cors());
