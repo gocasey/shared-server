5dc31755df92b434c487aafe51fecd1aead79df6
gocasey <gonzak6@gmail.com>
2018-04-14 00:21:51 -0300

    Fix some bugs related to schema validation

--- src/utils/schema_validator.js
+++ src/utils/schema_validator.js
@@ -3,9 +3,10 @@ const path = require('path');
 const Validator = require('jsonschema').Validator;
 
 function SchemaValidator() {
-    const schemasDirectory = '../models/schemas';
+    const schemasDirectory = path.join(__dirname, '..', 'models', 'schemas');
 
     this.validateJson = function(jsonToValidate, schemaFilename, callback) {
+        if (! jsonToValidate) return callback('undefined json to validate');
         let validator = new Validator();
         let schemaFilePath = path.join(schemasDirectory, schemaFilename);
         fs.readFile(schemaFilePath, 'utf8', function(err, data) {
