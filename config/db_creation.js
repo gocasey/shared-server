c21c14f5262c28611ec85a67ffe98dac4d469b01
gocasey <gonzak6@gmail.com>
2018-04-16 17:25:18 -0300

    Splitting connection string in two lines

--- config/db_creation.js
+++ config/db_creation.js
@@ -1,5 +1,6 @@
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://pqjyeqaijafusn:e98fa09f1a4e049674037a98dc4c1f3a956702400f306f9395a280923f38d7c0@ec2-54-163-240-54.compute-1.amazonaws.com:5432/dbhchlmki72u4a?ssl=true';
+const connectionString = process.env.DATABASE_URL || 'postgres://pqjyeqaijafusn:e98fa09f1a4e049674037a98dc4c1f3a956702400f306f9395a280923f38d7c0' +
+  '@ec2-54-163-240-54.compute-1.amazonaws.com:5432/dbhchlmki72u4a?ssl=true';
 const client = new pg.Client({
   connectionString: connectionString,
 });
--- src/lib/factories/connection_pool_factory.js
+++ src/lib/factories/connection_pool_factory.js
@@ -1,11 +1,13 @@
 const Pool = require('pg').Pool;
+const connectionString = process.env.DATABASE_URL || 'postgres://pqjyeqaijafusn:e98fa09f1a4e049674037a98dc4c1f3a956702400f306f9395a280923f38d7c0' +
+  '@ec2-54-163-240-54.compute-1.amazonaws.com:5432/dbhchlmki72u4a?ssl=true';
 
 function ConnectionPoolFactory(logger) {
     let _logger = logger;
 
     this.createPool = function() {
         let connectionSettings = {
-          connectionString: process.env.DATABASE_URL || 'postgres://pqjyeqaijafusn:e98fa09f1a4e049674037a98dc4c1f3a956702400f306f9395a280923f38d7c0@ec2-54-163-240-54.compute-1.amazonaws.com:5432/dbhchlmki72u4a?ssl=true',
+          connectionString: connectionString,
         };
         let pool = new Pool(connectionSettings);
         _logger.debug('Database connection settings: %j', connectionSettings);
