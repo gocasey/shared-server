0441cd36459f063e83618a4110376e00110cc3f9
gocasey <gonzak6@gmail.com>
2018-04-18 21:34:43 -0300

    Error middleware tests

---
+++ test/src/middlewares/error_middleware_tests.js
@@ -0,0 +1,57 @@
+const expect = require('expect.js');
+const errorMiddleware = require('../../../src/middlewares/error-middleware.js');
+
+describe('ErrorMiddleware Tests', function(){
+
+  let passedErrorCode;
+  let passedError;
+
+  let mockRequest = {};
+  let mockResponse = {
+    status: function(errorCode) {
+      passedErrorCode = errorCode;
+      return {
+        json: function(error) {
+          passedError = error;
+        },
+      }
+    }
+  };
+
+  describe('error with status code', function(){
+    let mockError = {
+      statusCode: 400,
+      message: 'Error message',
+    };
+
+    beforeEach(function() {
+      errorMiddleware(mockError, mockRequest, mockResponse, null);
+    });
+
+    it('passes error code to status', function() {
+      expect(passedErrorCode).to.be(400);
+    });
+
+    it('passes error object to json', function() {
+      expect(passedError).to.eql({ 'code': 400, 'message': 'Error message' });
+    });
+  });
+
+  describe('error without status code', function(){
+    let mockError = {
+      message: 'Error message',
+    };
+
+    beforeEach(function() {
+      errorMiddleware(mockError, mockRequest, mockResponse, null);
+    });
+
+    it('passes error code to status', function() {
+      expect(passedErrorCode).to.be(500);
+    });
+
+    it('passes error object to json', function() {
+      expect(passedError.message).to.be('Error message');
+    });
+  });
+});
