e39fa895bf5de95c76f5d7b4aa9fc9596515a986
gocasey <gonzak6@gmail.com>
2018-04-18 22:56:53 -0300

    UserRegistrationResponseBuilder tests

---
+++ test/src/middlewares/response_builders/user_registration_response_builder_tests.js
@@ -0,0 +1,39 @@
+const expect = require('expect.js');
+const sinon = require('sinon');
+const UserRegistrationResponseBuilder = require('../../../../src/middlewares/response_builders/user_registration_response_builder.js');
+
+const mockLogger = {
+  debug: sinon.stub(),
+};
+
+describe('UserRegistrationResponseBuilder Tests', function() {
+  let userRegistrationResponseBuilder = new UserRegistrationResponseBuilder(mockLogger);
+
+  describe('#buildResponse', function() {
+    let mockRequest = {};
+    let mockResponse = {
+      data: {
+        user_id: 123456789,
+        username: 'username',
+      },
+      json: sinon.stub(),
+    };
+
+    beforeEach(function() {
+      mockLogger.debug.resetHistory();
+      userRegistrationResponseBuilder.buildResponse(mockRequest, mockResponse);
+    });
+
+    it('passes response', function() {
+      expect(mockResponse.json.calledWith(sinon.match({ metadata: { version: '1.0.0' },
+        user: { id: 123456789, _rev: 'mockRev', applicationOwner: 'mockAppOwner', username: 'username' } })));
+    });
+
+    it('logs response', function() {
+      expect(mockLogger.debug.calledOnce);
+      expect(mockLogger.debug.getCall(0).args[0]).to.be('Response: %j');
+      expect(mockLogger.debug.getCall(0).args[1]).to.be.eql( { metadata: { version: '1.0.0' },
+        user: { id: 123456789, _rev: 'mockRev', applicationOwner: 'mockAppOwner', username: 'username' } } );
+    });
+  });
+});
