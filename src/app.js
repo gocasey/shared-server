7e327cbe290619cfcee3ce909ef94e2857aa5916
gocasey <gonzak6@gmail.com>
2018-06-20 20:43:41 -0300

    Logging error on main error handler

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const filesRouter = require('./middlewares/routers/files_router.js');
 const serversRouter = require('./middlewares/routers/servers_router.js');
 const usersRouter = require('./middlewares/routers/users_router.js');
 const ConnectionPoolFactory = require('./lib/factories/connection_pool_factory.js');
-const errorMiddleware = require('./middlewares/error_middleware.js');
+const errorMiddleware = require('./middlewares/main_error_handler.js');
 
 const logger = new Logger();
 const postgrePool = new ConnectionPoolFactory(logger).createPool();
@@ -27,7 +27,7 @@ serversRouter(app, logger, postgrePool);
 usersRouter(app, logger, postgrePool);
 
 // Add basic error middleware to handle all errors
-app.use(errorMiddleware);
+errorMiddleware(app, logger);
 
 // Start the app in the designated port and host
 
